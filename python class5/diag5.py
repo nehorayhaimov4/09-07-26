6dd6b939bfd6ff88830d87be213b4ebedadd2d66
nehorayhaimov4 <neorai3372@gmail.com>
2026-07-14 15:03:54 +0300

    .

--- python class5/diag5.py
+++ python class5/diag5.py
@@ -2,18 +2,13 @@
 
 num=int(input("Enter a two-digit number:"))
 if 10<=num<=99:
-    #YES
     tens=num//10
     ones=num%10
+    if tens == ones:
+        print("tens equals ones")
+    else:
+        print("tens not equals ones")
 else:
-    #NO
     print("number should be between 10-99")
-
-if tens==ones:
-    #YES
-    print("tens equals ones")
-else:
-    #NO
-    print("tens not equals ones")
 
 #STOP
